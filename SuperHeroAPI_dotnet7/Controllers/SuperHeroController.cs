5bac3a8c9251e12ba5a18c222631ed1a181f3353
tony tolver <andrademaresantonio00@gmail.com>
2023-01-14 22:07:34 -0600

    Get SuperHero

---
+++ SuperHeroAPI_dotnet7/Controllers/SuperHeroController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI_dotnet7.Models;
+
+namespace SuperHeroAPI_dotnet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var superHeroes = new List<SuperHero>()
+            {
+                new SuperHero
+                {
+                    Id=1,
+                    Name="Spider Man",
+                    FirstName="Peter",
+                    LastName="Parker",
+                    Place="New York"
+                },
+                new SuperHero
+                {
+                    Id=2,
+                    Name="Punisher",
+                    FirstName="Frank",
+                    LastName="Castle",
+                    Place="Texas"
+                }
+            };
+            return Ok(superHeroes);
+        }
+    }
+}
