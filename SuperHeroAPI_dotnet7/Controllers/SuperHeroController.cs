55501bfb8d097ccfbbfdd9f4c0683280495d1033
tony tolver <andrademaresantonio00@gmail.com>
2023-01-14 22:13:52 -0600

    get by id --- superheroes

--- SuperHeroAPI_dotnet7/Controllers/SuperHeroController.cs
+++ SuperHeroAPI_dotnet7/Controllers/SuperHeroController.cs
@@ -8,29 +8,53 @@ namespace SuperHeroAPI_dotnet7.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> superHeroes = new List<SuperHero>()
+        {
+            new SuperHero
+            {
+                Id=1,
+                Name="Spider Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="New York"
+            },
+            new SuperHero
+            {
+                Id=2,
+                Name="Punisher",
+                FirstName="Frank",
+                LastName="Castle",
+                Place="Texas"
+            },
+            new SuperHero
+            {
+                Id=3,
+                Name="Iron Main",
+                FirstName="Tony",
+                LastName="Stark",
+                Place="Las Vegas"
+            }
+        };
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            var superHeroes = new List<SuperHero>()
-            {
-                new SuperHero
-                {
-                    Id=1,
-                    Name="Spider Man",
-                    FirstName="Peter",
-                    LastName="Parker",
-                    Place="New York"
-                },
-                new SuperHero
-                {
-                    Id=2,
-                    Name="Punisher",
-                    FirstName="Frank",
-                    LastName="Castle",
-                    Place="Texas"
-                }
-            };
             return Ok(superHeroes);
+        }
+
+        //single hero
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetSingleHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+
+            if (hero == null)
+            {
+                return NotFound("Sorry, but this hero doesn´t exist");
+            }
+
+            return Ok(hero);
+
         }
     }
 }
